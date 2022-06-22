de990f0f0aef272c5a774272d4c8def483c62fbe
fx12138 <991773829@qq.com>
2022-06-22 15:16:43 +0800

    代码分割优化

--- src/index.js
+++ src/index.js
@@ -17,6 +17,15 @@ const block = document.createElement("div")
 block.textContent = '哈哈哈哈'
 block.classList.add('block-bg')
 document.body.appendChild(block)
+img.onclick = function () {
+  import(/*webpackChunkName:'math'*/"./other.js")
+    .then(res => {
+      console.log('模块加载成功', res)
+    })
+    .catch(err => {
+      console.log("模块加载失败", err)
+    })
+}
 
 if (module.hot) {
   //判断是否支持热模块替换功能
